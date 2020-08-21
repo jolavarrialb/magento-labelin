eaf960b973da25ec0739f9e4255ff79d8e4b34ab
Shimovolos, Aleksandr <a.shimovolos@itransition.com>
2020-08-21 10:15:49 +0300

    [LABELIN-5] - initial commit

---
+++ ThemeLabelin/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types = 1);
+
+use Magento\Framework\Component\ComponentRegistrar as Registrar;
+
+Registrar::register(Registrar::THEME, "frontend/Labelin/ThemeLabelin", __DIR__);
