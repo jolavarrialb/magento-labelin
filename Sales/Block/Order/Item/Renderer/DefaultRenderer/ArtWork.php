50e434f1afca0f75444f0b967e9d3a0432e32b9d
Lapa, Aleksey <a.lapa@itransition.com>
2020-09-04 16:26:33 +0300

    [LABELIN-37][feat][code] to add image to front order viev render

--- Sales/Block/Order/Item/Renderer/DefaultRenderer/ArtWork.php
+++ Sales/Block/Order/Item/Renderer/DefaultRenderer/ArtWork.php
@@ -4,32 +4,88 @@ declare(strict_types=1);
 
 namespace Labelin\Sales\Block\Order\Item\Renderer\DefaultRenderer;
 
-use Labelin\Sales\Helper\Config\ArtworkImgOptions as ArtworkHelper;
+use Labelin\Sales\Helper\Config\ArtworkOptions as ArtworkHelper;
+use Magento\Catalog\Model\Product\OptionFactory;
 use Magento\Framework\View\Element\Template;
 use Magento\Sales\Model\Order\Item;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Framework\UrlInterface;
 
 class ArtWork extends \Magento\Framework\View\Element\Template
 {
     /** @var ArtworkHelper */
     protected $artworkHelper;
 
+    /** @var OptionFactory  */
+    protected $productOptionFactory;
+
+    /**@var array|mixed|null */
+    protected $item = null;
+
+    /** @var array|mixed|null */
+    protected $artworkOption;
+
+    /** @var Json|mixed|null */
+    private $serializer;
+
+    /** @var array|bool|float|int|mixed|string|null */
+    protected $optionValues;
+
+    /** @var UrlInterface */
+    protected $url;
+
+
     /**
-     * @var Item | null
+     * ArtWork constructor.
+     * @param Template\Context $context
+     * @param ArtworkHelper $artworkHelper
+     * @param Json|null $serializer
+     * @param UrlInterface $url
+     * @param array $data
      */
-    protected $item;
-
-    public function __construct(Template\Context $context, array $data = [], ArtworkHelper $artworkHelper)
-    {
-
+    public function __construct(
+        Template\Context $context,
+        ArtworkHelper $artworkHelper,
+        UrlInterface $url,
+        Json $serializer = null,
+        array $data = []
+    ) {
+        $this->serializer = $serializer ?: \Magento\Framework\App\ObjectManager::getInstance()->get(Json::class);
         $this->artworkHelper = $artworkHelper;
-        $this->item = $this->getItem() ?? null;
+        $this->url = $url;
         parent::__construct($context, $data);
     }
 
-    protected function getComponentOptions()
+    public function parseItemOptions()
     {
-        return $this->artworkHelper->getArtworkComponentScopeConfigOptions();
+        $option = $this->getData('option');
+        $this->optionValues = $this->serializer->unserialize($option['option_value']);
+    }
 
+    public function getArtworkType(): string
+    {
+        return $this->optionValues['type'] ?? "";
+    }
+
+    public function getArtworkUrl(): string
+    {
+        $optionUrl = $this->optionValues['url'];
+        return $this->url->getUrl($optionUrl['route'], $optionUrl['params']) ?? '#';
+    }
+
+    public function getArtworkLabel(): string
+    {
+        return $this->optionValues['title'];
+    }
+
+    public function getArtworkWidth(): int
+    {
+        return $this->artworkHelper->getConfigHeight();
+    }
+
+    public function getArtworkHeight(): int
+    {
+        return $this->artworkHelper->getConfigWidth();
     }
 
 }
--- Sales/Helper/Config/ArtworkOptions.php
+++ Sales/Helper/Config/ArtworkOptions.php
@@ -5,26 +5,27 @@ namespace Labelin\Sales\Helper\Config;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Store\Model\ScopeInterface;
 
-class ArtworkImgOptions extends AbstractHelper
+class ArtworkOptions extends AbstractHelper
 {
     protected const XML_PATH_ARTWORK_ORDER_ITEM_GRID_WIDTH = 'labelin_sales/artwork_configuration/order_items_grid_width_size';
     protected const XML_PATH_ARTWORK_ORDER_ITEM_GRID_HEIGHT = 'labelin_sales/artwork_configuration/order_items_grid_height_size';
 
-
-    public function getArtworkComponentScopeConfigOptions($storeId = null): array
-    {
-        return [
-            'width' => $this->getScopeConfigOptions(static::XML_PATH_ARTWORK_ORDER_ITEM_GRID_WIDTH, $storeId),
-            'height' => $this->getScopeConfigOptions(static::XML_PATH_ARTWORK_ORDER_ITEM_GRID_HEIGHT, $storeId),
-        ];
-    }
-
-    protected function getScopeConfigOptions($xmlPatch, $storeId): int
+    public function getConfigWidth($storeId = null): int
     {
         return (int)$this->scopeConfig->getValue(
-            $xmlPatch,
+            static::XML_PATH_ARTWORK_ORDER_ITEM_GRID_WIDTH,
             ScopeInterface::SCOPE_STORE,
             $storeId
         );
     }
+
+    public function getConfigHeight($storeId = null): int
+    {
+        return (int)$this->scopeConfig->getValue(
+            static::XML_PATH_ARTWORK_ORDER_ITEM_GRID_HEIGHT,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
 }
