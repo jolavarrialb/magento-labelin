f656f57a7eb9984f8d47a906f7ae45b6c1726f68
shimovolos <a.shimovolos@itransition.com>
2020-09-21 18:44:16 +0300

    [LABELIN-50] - add steps info section

--- ConfigurableProduct/view/frontend/web/js/sticker-steps.js
+++ ConfigurableProduct/view/frontend/web/js/sticker-steps.js
@@ -1,5 +1,6 @@
 document.addEventListener('swatch-full-render', function () {
-    let stepsSelectors = document.querySelectorAll('div[data-step]');
+    let stepsSelectors = document.querySelectorAll('div[data-step]'),
+        stickerStepsWrapper = document.getElementById('sticker-steps-wrapper');
 
     document
         .getElementById('sticker_price')
@@ -7,6 +8,16 @@ document.addEventListener('swatch-full-render', function () {
     document
         .getElementById('sticker_artwork')
         .setAttribute('data-step', stepsSelectors.length + 2);
+
+    document.querySelectorAll('div[data-step]').forEach(function (element) {
+        let stepDiv = document.createElement('div');
+
+        stepDiv.className = 'sticker-step-info';
+        stepDiv.setAttribute('data-step-wrapper', element.getAttribute('data-step'));
+        stickerStepsWrapper.append(stepDiv);
+    });
+
+    processCurrentStepWrapper();
 });
 
 document.addEventListener('swatch-select-option', function () {
@@ -31,13 +42,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     backStep.addEventListener('click', function () {
-        let currentStep = parseInt(localStorage.getItem('sticker_current_step'));
+        let currentStep = parseInt(localStorage.getItem('sticker_current_step')),
+            isAvailableSwatchOption = document.querySelector('div[data-step="' + (currentStep - 1) + '"] .swatch-option'),
+            selectedSwatchOption = document.querySelector('div[data-step="' + (currentStep - 1) + '"] .swatch-option.selected');
 
         proceedStep(currentStep - 1);
 
+        processCurrentStepWrapper();
+
         nextStep.disabled = false;
-        if (document.querySelector('div[data-step="' + (currentStep - 1) + '"] .swatch-option')) {
-            nextStep.disabled = !document.querySelector('div[data-step="' + (currentStep - 1) + '"] .swatch-option.selected');
+        if (isAvailableSwatchOption) {
+            nextStep.disabled = !selectedSwatchOption;
         }
 
         if (isFirstStep()) {
@@ -51,13 +66,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     nextStep.addEventListener('click', function () {
-        let currentStep = parseInt(localStorage.getItem('sticker_current_step'));
+        let currentStep = parseInt(localStorage.getItem('sticker_current_step')),
+            isAvailableSwatchOption = document.querySelector('div[data-step="' + (currentStep + 1) + '"] .swatch-option'),
+            selectedSwatchOption = document.querySelector('div[data-step="' + (currentStep + 1) + '"] .swatch-option.selected');
 
         proceedStep(currentStep + 1);
 
+        processCurrentStepWrapper();
+
         this.disabled = false;
-        if (document.querySelector('div[data-step="' + (currentStep + 1) + '"] .swatch-option')) {
-            this.disabled = !document.querySelector('div[data-step="' + (currentStep + 1) + '"] .swatch-option.selected');
+        if (isAvailableSwatchOption) {
+            this.disabled = !selectedSwatchOption;
         }
 
         if (isLastStep()) {
@@ -69,34 +88,52 @@ document.addEventListener('DOMContentLoaded', function () {
             showBtn(backStep);
         }
     });
-
-    function proceedStep(stepId) {
-        let currentStep = parseInt(localStorage.getItem('sticker_current_step'));
-
-        document.querySelector('div[data-step="' + currentStep + '"]').style.display = 'none';
-        document.querySelector('div[data-step="' + (stepId) + '"]').style.display = 'block';
-
-        localStorage.removeItem('sticker_current_step');
-        localStorage.setItem('sticker_current_step', stepId);
-    }
-
-    function isFirstStep() {
-        return parseInt(localStorage.getItem('sticker_current_step')) === 1;
-    }
-
-    function isLastStep() {
-        return document.querySelectorAll('div[data-step]').length === parseInt(localStorage.getItem('sticker_current_step'));
-    }
-
-    function hideBtn(element) {
-        element.style.display = 'none';
-        element.disabled = true;
-    }
-
-    function showBtn(element) {
-        element.style.display = 'block';
-        element.disabled = false;
-    }
 });
+
+function proceedStep(stepId) {
+    let currentStep = parseInt(localStorage.getItem('sticker_current_step'));
+
+    document.querySelector('div[data-step="' + currentStep + '"]').style.display = 'none';
+    document.querySelector('div[data-step="' + (stepId) + '"]').style.display = 'block';
+
+    localStorage.removeItem('sticker_current_step');
+    localStorage.setItem('sticker_current_step', stepId);
+}
+
+function isFirstStep() {
+    return parseInt(localStorage.getItem('sticker_current_step')) === 1;
+}
+
+function isLastStep() {
+    return document.querySelectorAll('div[data-step]').length === parseInt(localStorage.getItem('sticker_current_step'));
+}
+
+function hideBtn(element) {
+    element.style.display = 'none';
+    element.disabled = true;
+}
+
+function showBtn(element) {
+    element.style.display = 'block';
+    element.disabled = false;
+}
+
+function processCurrentStepWrapper() {
+    let currentStep = parseInt(localStorage.getItem('sticker_current_step')),
+        currentStepWrapper = document.querySelector('.sticker-step-info[data-step-wrapper="' + currentStep + '"]'),
+        nextStepWrapper = document.querySelector('.sticker-step-info[data-step-wrapper="' + (currentStep + 1) + '"]'),
+        swatchLabel = document.querySelector('div[data-step="' + currentStep + '"] .swatch-attribute-label'),
+        stepWrapperLabel = 'Step ' + currentStep + ': ' + swatchLabel.innerHTML;
+
+    if (!isLastStep()) {
+        nextStepWrapper.classList.remove('info-active');
+        nextStepWrapper.classList.add('info-nonactive');
+    }
+
+    currentStepWrapper.classList.remove('info-nonactive');
+    currentStepWrapper.classList.add('info-active');
+
+    document.getElementById('sticker-steps-wrapper-info').innerText = stepWrapperLabel;
+}
 
 
