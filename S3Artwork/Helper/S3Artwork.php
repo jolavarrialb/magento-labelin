82a09e1a6aaffac8bb2697c642907b8c2ce1e40d
Lapa, Aleksey <a.lapa@itransition.com>
2020-11-17 18:35:46 +0300

    [LABELIN-124][feat][add] wirte files helper and logger

--- S3Artwork/Helper/S3Artwork.php
+++ S3Artwork/Helper/S3Artwork.php
@@ -4,18 +4,26 @@ declare(strict_types=1);
 
 namespace Labelin\S3Artwork\Helper;
 
+use Exception;
+use Labelin\Sales\Helper\ArtworkPreview;
 use Labelin\Sales\Model\Order\Item;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
 use Magento\Framework\Filesystem\Io\File;
-use Magento\Tests\NamingConvention\true\bool;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class S3Artwork extends AbstractHelper
 {
+    protected const S3_ARTWORK_DEFAULT_PATH = '/S3/';
+    protected const S3_ARTWORK_OPTIONS_PATH = 'labelin_s3_artwork/options/save_path';
+
     /** @var Filesystem */
     protected $fileSystem;
 
@@ -28,11 +36,19 @@ class S3Artwork extends AbstractHelper
     /** @var LoggerInterface */
     protected $logger;
 
+    /** @var StoreManagerInterface */
+    protected $storeManager;
+
+    /** @var ArtworkPreview */
+    protected $artworkPreviewHelper;
+
     public function __construct(
         Context $context,
         LoggerInterface $logger,
         Filesystem $fileSystem,
         DirectoryList $directoryList,
+        StoreManagerInterface $storeManager,
+        ArtworkPreview $artworkPreviewHelper,
         File $filesystemIo
     ) {
         parent::__construct($context);
@@ -40,20 +56,27 @@ class S3Artwork extends AbstractHelper
         $this->directoryList = $directoryList;
         $this->fileSystem = $fileSystem;
         $this->filesystemIo = $filesystemIo;
+        $this->storeManager = $storeManager;
+        $this->artworkPreviewHelper = $artworkPreviewHelper;
         $this->logger = $logger;
     }
 
+    /**
+     * @param Item $item
+     * @return bool
+     * @throws Exception
+     */
     public function saveArtworkToS3Folder(Item $item): bool
     {
-        $destination = $this->getS3Destination();
+        $destination = preg_replace('~[\/]{2,}~', '/', $this->getPath($item));
         $sourceImage = $this->getArtworkPath($item);
-        $resultImage = $this->getS3Destination($item);
+        $resultImage = preg_replace('~[\/]{2,}~', '/', $this->getS3ArtworkPath($item));
 
         try {
             $result = false;
 
             if ($this->filesystemIo->checkAndCreateFolder($destination)) {
-                $result = $this->filesystemIo->cp($sourceImage, $resultImage);
+                $result = $this->filesystemIo->write($resultImage, $sourceImage);
             }
 
             if (!$result) {
@@ -67,17 +90,41 @@ class S3Artwork extends AbstractHelper
         return $result;
     }
 
-
     /**
-     * @param null|Item $item
+     * @param Item $item
      * @return string
      * @throws FileSystemException
+     * @throws NoSuchEntityException
      */
-    public function getS3Destination($item = null): string
+    public function getPath(Item $item): string
     {
-        $imageName = null === $item ? '' : $this->getFileName($item);
+        return sprintf('%s%s%s', $this->getPubMedia()->getAbsolutePath(), $this->getS3Path(), $this->getOrderItemFolderPath($item));
+    }
 
-        return sprintf('%s%s%s', $this->getMedia()->getAbsolutePath(), static::DESTINATION_FOLDER_IMAGE, $imageName);
+    /**
+     * @return string
+     * @throws NoSuchEntityException
+     */
+    protected function getS3Path(): string
+    {
+        $configPath = $this->scopeConfig->getValue(
+            static::S3_ARTWORK_OPTIONS_PATH,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+
+        return $configPath ? $configPath : static::S3_ARTWORK_DEFAULT_PATH;
+    }
+
+    /**
+     * @param Item $item
+     * @return string
+     * @throws FileSystemException
+     * @throws NoSuchEntityException
+     */
+    public function getS3ArtworkPath(Item $item): string
+    {
+        return sprintf('%s%s', $this->getPath($item), $this->getFileName($item));
     }
 
     /**
@@ -89,7 +136,7 @@ class S3Artwork extends AbstractHelper
     {
         $originalImagePath = $this->artworkPreviewHelper->getArtworkOptionsPathByItem($item);
 
-        return sprintf('%s%s', $this->getMedia()->getAbsolutePath(), $originalImagePath);
+        return sprintf('%s%s', $this->getPubMedia()->getAbsolutePath(), $originalImagePath);
     }
 
     /**
@@ -99,7 +146,7 @@ class S3Artwork extends AbstractHelper
      */
     public function getArtworkOptionsPathByItem(Item $item): string
     {
-        $imageFilePath = $this->getS3Destination($item);
+        $imageFilePath = $this->getS3ArtworkPath($item);
         $fileExist = $this->filesystemIo->fileExists($imageFilePath);
 
         return $fileExist ? $imageFilePath : '';
@@ -112,5 +159,25 @@ class S3Artwork extends AbstractHelper
 
         return sprintf('%s_%s_%s', $orderId, $item->getId(), $fileName);
     }
+
+    /**
+     * @return WriteInterface
+     * @throws FileSystemException
+     */
+    public function getPubMedia(): WriteInterface
+    {
+        return $this->fileSystem->getDirectoryWrite($this->directoryList::PUB);
+    }
+
+
+    /**
+     * @param Item|null $item
+     * @return string
+     */
+    protected function getOrderItemFolderPath(Item $item): string
+    {
+        return sprintf('/orderId_%s/itemId_%s/', $item->getOrderId(), $item->getId());
+    }
+
 
 }
