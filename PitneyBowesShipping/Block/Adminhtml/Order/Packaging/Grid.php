aa5d02674daab8f0ae218b20283de9daade97a32
Lapa, Aleksey <a.lapa@itransition.com>
2020-12-16 10:47:56 +0300

    [LABELIN-147][feat][fix] grid product item name to SKU

---
+++ PitneyBowesShipping/Block/Adminhtml/Order/Packaging/Grid.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Labelin\PitneyBowesShipping\Block\Adminhtml\Order\Packaging;
+
+class Grid extends \Magento\Shipping\Block\Adminhtml\Order\Packaging\Grid
+{
+    /** @var string  */
+    protected $_template = 'Labelin_PitneyBowesShipping::order/packaging/grid.phtml';
+}
