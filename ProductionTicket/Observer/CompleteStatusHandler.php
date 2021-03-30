d65fe7ce9105b3eb14e2df3c076764d6b3d94009
Lapa, Aleksey <a.lapa@itransition.com>
2021-03-30 18:44:31 +0300

    [LABELIN-205][feat][fix] order ready to ship status only if all items from order is in production ticket and is in complete status

--- ProductionTicket/Observer/CompleteStatusHandler.php
+++ ProductionTicket/Observer/CompleteStatusHandler.php
@@ -10,7 +10,6 @@ use Labelin\Sales\Model\Order;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Framework\Exception\LocalizedException;
 use Magento\Sales\Api\OrderRepositoryInterface;
 
 class CompleteStatusHandler implements ObserverInterface
@@ -23,6 +22,9 @@ class CompleteStatusHandler implements ObserverInterface
 
     /** @var SearchCriteriaBuilder */
     protected $searchCriteriaBuilder;
+
+    /** @var Order */
+    protected $order;
 
     public function __construct(
         OrderRepositoryInterface $orderRepository,
@@ -43,18 +45,31 @@ class CompleteStatusHandler implements ObserverInterface
             return $this;
         }
 
+        $this->order = $this->orderRepository->get($productionTicket->getOrderId());
+
         $searchCriteria = $this->searchCriteriaBuilder
             ->addFilter('order_id', $productionTicket->getOrderId(), 'eq')
             ->addFilter('status', 0, 'eq')
             ->create();
 
-        if (!$this->productionTicketRepository->getList($searchCriteria)->getTotalCount()) {
-            /** @var Order $order */
-            $order = $this->orderRepository->get($productionTicket->getOrderId());
-            $order->markAsReadyToShip();
-            $this->orderRepository->save($order);
+        if (!$this->productionTicketRepository->getList($searchCriteria)->getTotalCount()
+            && $this->compareOrderAndProductionTicketItemsQty()
+        ) {
+            $this->order->markAsReadyToShip();
+            $this->orderRepository->save($this->order);
         }
 
         return $this;
+    }
+
+    protected function compareOrderAndProductionTicketItemsQty(): bool
+    {
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->addFilter('order_id', $this->order->getId())
+            ->create();
+        $productionTicketItemsCount = $this->productionTicketRepository->getList($searchCriteria)->getTotalCount();
+        $orderItemsCount = count($this->order->getAllVisibleItems());
+
+        return $productionTicketItemsCount === $orderItemsCount;
     }
 }
